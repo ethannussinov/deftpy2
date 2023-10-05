9ec3720d71512fa6d6689a9e360ec5af021735ac
avi-i <a.isakov@wustl.edu>
2023-10-05 14:54:35 -0500

    alter get_witman_data.py to include ternaries and the potential to add sigma Eb for regressor and visualization.

--- get_witman_data.py
+++ get_witman_data.py
@@ -13,10 +13,13 @@ from deftpy.crystal_analysis import Crystal
 
 
 def main():
-    csv_paths = sorted(glob("playground/witman_data/data_01_03_22/csvs/*.csv"))
-    poscar_path = "playground/witman_data/data_01_03_22/poscars"
-
+    data_path = "/Users/isakov/Desktop/Fall_23/structures_production/data_01_03_22/"
+    #csv_paths = sorted(glob("playground/witman_data/data_01_03_22/csvs/*.csv"))
+    csv_paths = sorted(glob(data_path + "csvs/*.csv"))
+    #poscar_path = "playground/witman_data/data_01_03_22/poscars"
+    poscar_path = data_path + "poscars"
     # Read in all the data and concatenate it into one dataframe
+
     # Create a column for the csv file name, not including the path and .csv extension
     df = pd.concat(
         [pd.read_csv(csv_path).assign(filename=csv_path.split("/")[-1][:-4]) for csv_path in csv_paths]
@@ -40,7 +43,8 @@ def main():
 
     # Add oxidation states to the structure
     for _, row in df.iterrows():
-        oxstate_path = f"playground/witman_data/data_01_03_22/oxstate/{row['filename']}_oxstate"
+        #oxstate_path = f"playground/witman_data/data_01_03_22/oxstate/{row['filename']}_oxstate"
+        oxstate_path = data_path + f"oxstate/{row['filename']}_oxstate"
         oxstate = []
         for x in open(oxstate_path).read().split():
             oxstate += int(x.split("*")[0]) * [float(x.split("*")[1])]
@@ -48,14 +52,17 @@ def main():
         structure.add_oxidation_state_by_site(oxstate)
 
     # Binary?
-    df["is_binary"] = df["formula"].apply(lambda x: len(Composition(x).elements) == 2)
+    #df["is_binary"] = df["formula"].apply(lambda x: len(Composition(x).elements) == 2)
+    # Ternary?
+    df["is_binary_or_ternary"] = df["formula"].apply(lambda x: 2 <= len(Composition(x).elements) <= 3)
 
     # Sort by defectid and then by site
     df = df.sort_values(["defectid", "site"])
     # df.to_csv("oxygen_vacancies.csv", index=False)
 
     # Calculate crystal features for binary structures
-    df = df[df["is_binary"]]
+    #df = df[df["is_binary"]]
+    df = df[df["is_binary_or_ternary"]]
     df_cf = pd.DataFrame()
     for defectid in tqdm(df["defectid"].unique()):
         df_defectid = df[df["defectid"] == defectid]
@@ -107,19 +114,22 @@ def main():
         except ValueError:
             pass
     df_cf = df_cf.reset_index(drop=True)
-    df_cf.to_csv("witman_data.csv", index=False)
+    df_cf.to_csv("witman_data1.5.csv", index=False)
 
     # plot witman-based cfm
     # remove NaNs
     df_cf = df_cf.dropna()
     cfm = HuberRegressor()
-    X = df_cf[["Vr_max", "Eg"]]
+    #X = df_cf[["Vr_max", "Eg"]]
+    X = df_cf[["Eb_sum", "Vr_max", "Eg"]]
     y = df_cf["Ev"]
     cfm.fit(X, y)
     y_pred = cfm.predict(X)
+
     plt.scatter(y, y_pred)
     plt.plot([1, 9], [1, 9], "k--")
-    equation = f"$E_v$ = {cfm.intercept_:.2f} + {cfm.coef_[0]:.2f} $V_r$ + {cfm.coef_[1]:.2f} $E_g$"
+    #equation = f"$E_v$ = {cfm.intercept_:.2f} + {cfm.coef_[0]:.2f} $V_r$ + {cfm.coef_[1]:.2f} $E_g$"
+    equation = f"$E_v$ = {cfm.intercept_:.2f} + {cfm.coef_[0]:.2f} $\\Sigma E_b$ + {cfm.coef_[1]:.2f} $V_r$ + {cfm.coef_[2]:.2f} $E_g$"
     plt.text(1, 9, equation, fontsize=9)
     mae = np.mean(np.abs(y - y_pred))
     plt.text(1, 8, f"MAE = {mae:.2f} eV", fontsize=9)
@@ -130,7 +140,7 @@ def main():
     #     texts.append(plt.text(x, y, s, size=6))
     # adjustText.adjust_text(texts, arrowprops=dict(arrowstyle="-", color="k", lw=0.5))
 
-    plt.savefig("witman_fit.png", dpi=300)
+    plt.savefig("witman_fit1.5.png", dpi=300)
 
 
 if __name__ == "__main__":
